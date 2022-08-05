fe738d5d7583f208e4eb950b38243262dccfe3ea
Deny Kurniawan <dekakrens@gmail.com>
2022-08-06 05:21:42 +0700

    Add One Button library to simply debounce the button, add some function

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,71 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
+#include <OneButton.h>
 
-BleKeyboard blekeyboard;
+OneButton BTN_1(13, true, true);
+OneButton BTN_2(4, true, true);
+OneButton BTN_3(33, true, true);
+OneButton BTN_4(18, true, true);
+OneButton BTN_5(32, true, true);
+OneButton BTN_6(21, true, true);
+
+BleKeyboard blekeyboard("Macro Keyboard", "Espressif", 100);
+
+void BTN_1_CLICK() {
+  // You can specify certain function here
+}
+
+void BTN_2_CLICK() {
+  // You can specify certain function here
+}
+
+// Git Add and Git Commit
+void BTN_3_CLICK() {
+  blekeyboard.println("git add .");
+  delay(500);
+  blekeyboard.print("git commit -m \"\"");
+  blekeyboard.write(KEY_LEFT_ARROW);
+
+}
+
+void BTN_4_CLICK() {
+  // You can specify certain function here
+}
+
+// Mute/Unmute Google Meet
+void BTN_5_CLICK() {
+  blekeyboard.press(KEY_LEFT_CTRL);
+  blekeyboard.press(0x64);
+  delay(100);
+  blekeyboard.releaseAll();
+}
+
+// Switch Camera On/Off Google Meet
+void BTN_6_CLICK() {
+  blekeyboard.press(KEY_LEFT_CTRL);
+  blekeyboard.press(0x65);
+  delay(100);
+  blekeyboard.releaseAll();
+}
 
 void setup() {
-  Serial.begin(115200);
   blekeyboard.begin();
-  Serial.print("Starting BLE Work!");
+
+  BTN_1.attachClick(BTN_1_CLICK);
+  BTN_2.attachClick(BTN_2_CLICK);
+  BTN_3.attachClick(BTN_3_CLICK);
+  BTN_4.attachClick(BTN_4_CLICK);
+  BTN_5.attachClick(BTN_5_CLICK);
+  BTN_6.attachClick(BTN_6_CLICK);
 }
 
 void loop() {
-  if(blekeyboard.isConnected()){
-    Serial.println("Sending 'Hello World!");
-    blekeyboard.print("Hello World");
-    delay(1000);
-
-    Serial.println("Sending Enter key");
-    blekeyboard.press(KEY_LEFT_CTRL);
-    blekeyboard.press(0x61);
-    delay(100);
-    blekeyboard.releaseAll();
-    blekeyboard.write(KEY_BACKSPACE);
-    blekeyboard.end();
-    delay(1000);
-
-    Serial.println("Waiting 5 seconds...");
+  if(blekeyboard.isConnected()) {
+    BTN_1.tick();
+    BTN_2.tick();
+    BTN_3.tick();
+    BTN_4.tick();
+    BTN_5.tick();
+    BTN_6.tick();
   }
 }
